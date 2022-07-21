218ddf4749f65cee7321b255087140d1ccbf5de7
othiagomanhaes <othiagomanhaes@gmail.com>
2022-07-21 02:39:10 -0300

    consertando o exercício do par e adicionando o exercício do ímpar

--- Fundamentos/Bloco-4-introducao-JS-e-LogicProgram/dia-1-JavaScript-primeiros-passos/par.js
+++ Fundamentos/Bloco-4-introducao-JS-e-LogicProgram/dia-1-JavaScript-primeiros-passos/par.js
@@ -1,9 +1,9 @@
-const num1 = 1;
-const num2 = 3;
-const num3 = 5;
+const num1 = 2;
+const num2 = 7;
+const num3 = 3;
 
 
-if (num1 % 2 == 0 || num2 % 2 == 0 || num3 == 0) {
+if (num1 % 2 == 0 || num2 % 2 == 0 || num3 % 2 == 0) {
     console.log(true)
 }
 else {
