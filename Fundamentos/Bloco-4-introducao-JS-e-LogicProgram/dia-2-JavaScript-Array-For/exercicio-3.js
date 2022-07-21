af5e6821729e52cc9b7348f2ab6fa8fd4865d846
othiagomanhaes <othiagomanhaes@gmail.com>
2022-07-21 18:30:24 -0300

    resolução do ex 3 e enviando o ex2

--- Fundamentos/Bloco-4-introducao-JS-e-LogicProgram/dia-2-JavaScript-Array-For/exercicio-3.js
+++ Fundamentos/Bloco-4-introducao-JS-e-LogicProgram/dia-2-JavaScript-Array-For/exercicio-3.js
@@ -1,7 +1,8 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let soma = null;
+let media, soma = 0;
 
 for (let index = 0; index < numbers.length; index +=1){
     soma = soma + numbers[index];
 }
-console.log(soma);
+
+console.log(media = soma / numbers.length)
