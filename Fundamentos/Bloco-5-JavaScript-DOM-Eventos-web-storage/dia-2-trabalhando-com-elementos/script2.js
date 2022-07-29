0dcdb64cb984567da776e03170aecc9d634171b1
othiagomanhaes <othiagomanhaes@gmail.com>
2022-07-29 02:17:45 -0300

    Metade dos exercícios feitos.

--- Fundamentos/Bloco-5-JavaScript-DOM-Eventos-web-storage/dia-2-trabalhando-com-elementos/script2.js
+++ Fundamentos/Bloco-5-JavaScript-DOM-Eventos-web-storage/dia-2-trabalhando-com-elementos/script2.js
@@ -4,3 +4,26 @@ let paiDeTodos = document.getElementsByTagName('body');
 let titulo = document.createElement('h1');
 titulo.innerText = 'Exercício 5.2 - JavaScript DOM';
 paiDeTodos[0].appendChild(titulo);
+
+// 2-Adicione a tag main com a classe main-content como filho da tag body;
+
+let principal = document.createElement('main');
+principal.classList.add('main-content');
+document.body.appendChild(principal);
+
+// 3-Adicione a tag section com a classe center-content como filho da tag main criada no passo 2;
+let secao = document.createElement('section');
+secao.classList.add('center-content');
+principal[0].appendChild(secao);
+
+// 4-Adicione a tag p como filho do section criado no passo 3 e coloque algum texto;
+
+let paragrafo = document.createElement('p');
+paragrafo.innerText = "Dom é treino";
+secao.appendChild(paragrafo);
+
+// 5-Adicione a tag section com a classe left-content como filho da tag main criada no passo 2;
+let secao2 = document.createElement('section');
+secao2.classList.add('left-content');
+
+
