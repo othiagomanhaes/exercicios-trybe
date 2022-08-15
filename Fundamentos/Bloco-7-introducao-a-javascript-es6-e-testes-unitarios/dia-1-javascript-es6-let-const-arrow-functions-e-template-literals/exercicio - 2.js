ce2d1d1c06b438a4431a215941ee7791546c4a77
othiagomanhaes <othiagomanhaes@gmail.com>
2022-08-15 15:21:31 -0300

    Exercício 2 completo.

---
+++ Fundamentos/Bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercicio - 2.js
@@ -0,0 +1,8 @@
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+const sortOddsAndEvens =  oddsAndEvens.sort((a,b) => a - b);
+
+// Seu código aqui.
+
+
+console.log(sortOddsAndEvens); // será necessário alterar essa linha 😉
