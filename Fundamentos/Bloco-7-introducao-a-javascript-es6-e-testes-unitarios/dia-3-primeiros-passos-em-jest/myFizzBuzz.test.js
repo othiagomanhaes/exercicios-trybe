c61490f9087dea42c49a66d3a4f868fd4ec215cc
othiagomanhaes <othiagomanhaes@gmail.com>
2022-08-17 23:52:07 -0300

    Requisito 3 finalizado.

--- Fundamentos/Bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/myFizzBuzz.test.js
+++ Fundamentos/Bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/myFizzBuzz.test.js
@@ -1,7 +1,23 @@
 const myFizzBuzz = require('./myFizzBuzz');
 
 describe('Função myFizzBuzz', () => {
-    it('Verifica se a função retorna FizzBuzz quando recebe um número divisível por 3 e por 5', () => {
+    it('Verifica se a função retorna fizzbuzz quando recebe um número divisível por 3 e por 5', () => {
       expect(myFizzBuzz(15)).toMatch('fizzbuzz');
     });
+
+    it('Verifica se a função retorna fizz quando recebe um número apenas divisível por 3', () => {
+      expect(myFizzBuzz(6)).toMatch('fizz');
+    });
+
+    it('Verifica se a função retorna buzz quando recebe um número apenas divisível por 5', () => {
+      expect(myFizzBuzz(10)).toMatch('buzz');
+    });
+
+    it('Verifica se a função retorna o próprio número passado no praâmetro quando não for divisível por 3 e ou por 5.', () => {
+      expect(myFizzBuzz(7)).toEqual(7);
+    });
+
+    it('Verifica se a função retorna false quando não recebe um número no parâmetro.', () => {
+      expect(myFizzBuzz(!Number)).toBeFalsy();
+    })
 });
