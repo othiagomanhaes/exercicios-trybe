e55dbbb0a8a078a13d0cd65f875c6368c5614bd0
othiagomanhaes <othiagomanhaes@gmail.com>
2022-09-07 00:00:17 -0300

    Requisito 7 realizado.

---
+++ Front-end/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/exercicio7.js
@@ -0,0 +1,37 @@
+const pokemons = [
+    {
+      name: 'Bulbasaur',
+      type: 'Grama',
+      ability: 'Raio Solar',
+    },
+    {
+      name: 'Charmander',
+      type: 'Fogo',
+      ability: 'Lança Chamas',
+    },
+    {
+      name: 'Squirtle',
+      type: 'Água',
+      ability: 'Jato de Água',
+    },
+];
+  
+function getPokemonDetails(selectedPokemon, callback) {
+  const foundPokemon = pokemons.find((pokemon) => pokemon.name === selectedPokemon);
+  
+  setTimeout(() => {
+    if (foundPokemon === undefined) {
+      return callback(new Error('Não temos esse pokémon para você :('), null);
+    }
+  
+    const { name, type, ability } = foundPokemon;
+  
+    const messageFromProfOak = `Olá, seu pokémon é o ${name}, o tipo dele é ${type} e a habilidade principal dele é ${ability}`;
+  
+    callback(null, messageFromProfOak);
+  }, 2000);
+}
+  
+const handlePokemonSearch = (error, message) => (error) ? console.log(error) : console.log(message);
+  
+getPokemonDetails('Squirtle', handlePokemonSearch);
